b1539e1ba7b136a8b78c5cbe4ab766e00816eef9
GarciaRenan2 <renanfernando2@gmail.com>
2021-06-15 22:59:16 -0300

    First commitr

--- src/app/home/signup/signup.service.ts
+++ src/app/home/signup/signup.service.ts
@@ -1,14 +1,14 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
-const API = "http://localhost:3000"
+const API = 'http://localhost:3000';
 @Injectable({
   providedIn: 'root',
 })
 export class SignupService {
   constructor(private http: HttpClient) {}
 
-  checkUserNameTaken(userName: string){
-    
+  checkUserNameTaken(userName: string) {
+      return true;
   }
 }
